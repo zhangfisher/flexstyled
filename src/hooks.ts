d14799854917589e2e96e85807344238ba8f0d5d
zhangfisher <wxzhang@126.com>
2024-05-29 08:56:35 +0800

    fix: 修复useStyled重复注入样式的问题

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback,  useRef, useState,useInsertionEffect, useLayoutEffect, useEffect, EffectCallback } from "react";
+import { useCallback,  useRef, useState,useInsertionEffect, useLayoutEffect, useEffect, EffectCallback, useMemo } from "react";
 import { generateStyleId, insertStylesheet, removeStylesheet } from "./utils";
 import type { CSSRuleObject, StyledObject } from "./types";
 import { createStyled, type StyledOptions } from ".";
@@ -28,25 +28,17 @@ export function useOnMount(effect: EffectCallback) {
  * @param value 
  */
 export function useStyled<Props=any>(styles: CSSRuleObject<Props> | (()=>CSSRuleObject<Props>),options?:Omit<StyledOptions,'id'>):StyledObject {
-    const firstRef = useRef<boolean>(false); 
     const styleId = useRef<string>(''); 
-    const createStyle = useCallback(()=>{    
+    const styledObj = useMemo(()=>{    
         const styleData = typeof(styles)=='function' ? styles() : styles
         if(styleId.current=='') styleId.current = generateStyleId()
         const opts = Object.assign({ inject:false, id: styleId.current }, options) as unknown  as StyledOptions        
         return createStyled(styleData,opts)
     },[])
 
-    const [ styledObj ,setStyledObj] = useState<StyledObject>(createStyle())
-    const injectStyle = useCallback(()=>{    
+     const injectStyle = useCallback(()=>{    
         insertStylesheet(styledObj!.css,styledObj!.id)
     },[])
-    
-    if(!firstRef.current) {        
-        setStyledObj(createStyle())
-        injectStyle()
-        firstRef.current = true
-    }
     
     useInsertionEffect (() => {    
         injectStyle()   
